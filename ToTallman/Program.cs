d768f464f06da54515efa3a1be6384bf821f8950
MattCordell <matt@snoyowie.com>
2018-08-15 22:30:01 +1000

    Initial commit. Working PoC, sample app.

--- ToTallman/Program.cs
+++ ToTallman/Program.cs
@@ -3,13 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ToTallman
 {
     class Program
     {
+        public static Dictionary<string, string> Tallmen = new Dictionary<string, string>();
+
+
         static void Main(string[] args)
         {
+            var Drugs = Properties.Resources.MedicinalProductTerms.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var TM = Properties.Resources.Tallmen.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            //initialiseTallmen
+            foreach (var entry in TM)
+            {
+                AddTallman(entry);
+            }
+
+            Console.WriteLine("{0} => {1}", "amitriptyline", ToTallman("amitriptyline"));
+
+            foreach (var D in Drugs)
+            {
+                Console.WriteLine("{0} => {1}", D, ToTallman(D));
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+
+        }
+
+        public static void AddTallman(string s)
+        {
+            Tallmen.Add(s.ToLower(), s);
+        }
+
+        public static string ToTallman(string term)
+        {
+            var words = term.ToLower().Split(' ');
+
+            foreach (var word in words)
+            {
+                if (Tallmen.ContainsKey(word))
+                {
+                    string pattern = String.Format("({0}|{1}){2}", Char.ToUpper(word[0])
+                                                                 , word[0]
+                                                                 , word.Substring(1));
+                    Regex rgx = new Regex(pattern);
+                    term = rgx.Replace(term, Tallmen[word]);
+                }
+            }
+
+            return term;
         }
     }
 }
