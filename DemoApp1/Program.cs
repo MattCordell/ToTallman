a8e3488bc9ae9c88512cf18b25f9955a1a22e495
MattCordell <matt@snoyowie.com>
2018-08-17 06:02:30 +1000

    Redo/clean .net core demo app

---
+++ DemoApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ToTallman;
+
+
+//.Net Core Console App
+namespace DemoApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Drugs = Properties.Resources.MedicinalProductTerms.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var D in Drugs)
+            {
+                Console.WriteLine("{0} => {1}", D, D.ToTallman());
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
