1d5d791598cf1d35e0c74e19cb83345fd4c65c5e
MattCordell <matt@snoyowie.com>
2018-08-21 21:06:00 +1000

    tweek demo app

--- DemoApp1/DemoApp1.cs
+++ DemoApp1/DemoApp1.cs
@@ -17,6 +17,9 @@ namespace DemoApp1
                 Console.WriteLine("{0} => {1}", D, D.ToTallman());                                   
             }
 
+            var s1 = "This drug is norfloxacin";
+            Console.WriteLine("{0} => {1}", s1, s1.ToTallman(Tallman.List.AU));
+
             Console.WriteLine("Done");
             Console.ReadKey();
         }
